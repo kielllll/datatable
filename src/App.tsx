fcda9989d82351cca433cc9e0f2ec257606b09d2
kielllll <eatarranza@gmail.com>
2024-07-01 17:29:38 +0800

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <h1 className="text-slate-400">Hello There</h1>
+    </div>
+  )
+}
+
+export default App
