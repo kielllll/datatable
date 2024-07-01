ec067ba5717e5a7cfea16bd2ecc88a2b570dc84e
kielllll <eatarranza@gmail.com>
2024-07-01 17:50:58 +0800

    fix: revert on using api calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import DataTable from './components/datatable'
-import { useGetRandomUsers } from './hooks/useGetRandomUsers'
 
 function App() {
-  const { data } = useGetRandomUsers()
-
-  console.log(data)
-
   return (
     <div className="h-screen w-screen flex justify-center items-center">
       <h1 className="text-slate-400">Hello There</h1>
