18f6ee5e5fffca0f17f73e4f4979ba13e185a22a
kielllll <eatarranza@gmail.com>
2024-07-01 22:11:23 +0800

    fix: grid display

--- src/components/datatable/Row.tsx
+++ src/components/datatable/Row.tsx
@@ -2,5 +2,5 @@ interface RowProps {
   children: JSX.Element[]
 }
 export default function Row({ children }: RowProps) {
-  return <tr className="w-full flex justify-evenly">{children}</tr>
+  return <tr className="w-full grid grid-flow-col auto-cols-fr">{children}</tr>
 }
