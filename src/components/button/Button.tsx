25567151e4914d1622743cfa965c256f0628b004
kielllll <eatarranza@gmail.com>
2024-07-02 10:11:58 +0800

    fix: button props

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,9 +1,15 @@
 import { type ButtonProps, Button as HButton } from '@headlessui/react'
 import classNames from 'classnames'
 
-export default function Button({ children, onClick, className }: ButtonProps) {
+export default function Button({
+  children,
+  onClick,
+  className,
+  ...props
+}: ButtonProps) {
   return (
     <HButton
+      {...props}
       className={classNames(
         'inline-flex items-center justify-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white',
         className
